a1c4d8096c2d096971d548ea5671d4c591a804d2
guillaumejvrn <guillaume.jenvrin@sts-sio-caen.info>
2023-12-21 21:46:00 +0100

    louislerosie

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow fenetre(sf::VideoMode(800, 600), "SFML");
+
+    sf::Font police;
+    if (!police.loadFromFile("/home/pepito/Documents/font/Roboto-MediumItalic.ttf")) {
+        // Gérer l'erreur de chargement de la police
+    }
+
+    sf::Text texte;
+    texte.setFont(police);
+    texte.setString("Hello, SFML!");
+    texte.setCharacterSize(24);
+    texte.setFillColor(sf::Color::White);
+    texte.setPosition(10, 100);
+
+    while (fenetre.isOpen()) {
+        sf::Event evenement;
+        while (fenetre.pollEvent(evenement)) {
+            if (evenement.type == sf::Event::Closed) {
+                fenetre.close();
+            }
+        }
+
+        fenetre.clear();
+        fenetre.draw(texte);  // Dessinez le texte
+        fenetre.display();
+    }
+
     return 0;
 }
